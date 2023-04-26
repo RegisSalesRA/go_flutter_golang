bb98b8f087f262ba41f1d50b24b929f99cf3f17b
Régis Rommel <48102820+RegisSalesRA@users.noreply.github.com>
2023-04-26 14:56:13 -0300

    Delete server directory

--- server/models/models.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Task struct {
-	ID          int64  `json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Done        bool   `json:"done"`
-}
