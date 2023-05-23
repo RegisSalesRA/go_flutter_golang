396f39fd056cf17550d34d2d54f0eef86c69980f
RegisSalesRA <regismsales@gmail.com>
2023-05-23 16:41:29 -0300

    renamed routes

--- main.go
+++ main.go
@@ -16,16 +16,15 @@ func main() {
 	}
 
 	r := chi.NewRouter()
-	r.Get("/", query.List)
-	r.Get("/{id}", query.Get)
+	r.Get("/tasks/", query.List)
+	r.Get("/tasks/{id}", query.Get)
 
-	r.Post("/", query.Create)
+	r.Post("/tasks/", query.Create)
 
-	r.Put("/{id}", query.Update)
-	r.Put("/done/{id}", query.UpdateDataDone)
+	r.Put("/tasks/{id}", query.Update)
+	r.Put("/tasks/done/{id}", query.UpdateDataDone)
 
-	r.Delete("/{id}", query.Delete)
-
+	r.Delete("/tasks/{id}", query.Delete)
 
 	http.ListenAndServe(fmt.Sprintf(":%s", configs.GetServerPort()), r)
 
