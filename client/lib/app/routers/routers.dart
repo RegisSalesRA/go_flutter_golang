1e031b9390715a91b048d7adfb9bfcce78ede5dc
RegisSalesRA <regismsales@gmail.com>
2023-05-25 10:17:17 -0300

    update routes

--- client/lib/app/routers/routers.dart
+++ client/lib/app/routers/routers.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../../screens/create_task_form_screen.dart';
 import '../../screens/home_screen.dart';
  
 
@@ -8,12 +7,9 @@ class Routes {
   static Map<String, Widget Function(BuildContext)> list =
       <String, WidgetBuilder>{
     '/home': (context) => const HomeScreen(),
-    '/createTask': (context) => const CreateTaskFormScreen(),
   };
 
   static String home = '/home';
-  static String createTask = '/createTask';
-  static String done = '/done';
 
   static GlobalKey<NavigatorState>? navigatorKey = GlobalKey<NavigatorState>();
 }
