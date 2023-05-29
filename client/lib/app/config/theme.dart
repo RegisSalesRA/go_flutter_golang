77cf09e81e3d5cc6fc5679e28720d4e5adb94fd9
RegisSalesRA <regismsales@gmail.com>
2023-05-29 10:21:15 -0300

    theme data refactory color

--- client/lib/app/config/theme.dart
+++ client/lib/app/config/theme.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 ThemeData themeDataMethod() {
   return ThemeData(
+    primarySwatch: ColorsTheme.themeColor,
     iconTheme: const IconThemeData(color: ColorsTheme.iconColor),
     colorScheme: ColorScheme.fromSwatch(primarySwatch: ColorsTheme.themeColor)
         .copyWith(secondary: ColorsTheme.primaryColor),
